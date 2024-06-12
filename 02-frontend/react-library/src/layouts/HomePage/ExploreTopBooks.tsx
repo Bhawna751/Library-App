25b4ac5e850e459f5170c2a9f9e5281dc1e3d677
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-12 08:36:39 +0530

    Home page- Explore top Books component

---
+++ 02-frontend/react-library/src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,14 @@
+export const ExploreTopBooks = () =>{
+    return(
+        <div className="p-5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 text-white
+                d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Find Your Next Adventure</h1>
+                    <p className="col-md-8 fs-4">Where Would You Like To Go Next?</p>
+                    <a type='button' className='btn main-color btn-lg text-white' href='#'>Explore top Books</a>
+                </div>
+                </div>
+        </div>
+    );
+}
