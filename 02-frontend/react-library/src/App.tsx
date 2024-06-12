100ee9d549d4cd043f43f19d11800e4955dea079
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-12 16:14:13 +0530

    Home Page - Carousel Component

--- 02-frontend/react-library/src/App.tsx
+++ 02-frontend/react-library/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Carousel } from './layouts/HomePage/Carousel';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
     </div>
   );
 }
