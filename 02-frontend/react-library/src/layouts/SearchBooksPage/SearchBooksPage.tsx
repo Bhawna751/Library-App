d379fee68161d4e5bf591cc488a157582b785407
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-16 16:28:46 +0530

    Pagination

--- 02-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 02-frontend/react-library/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -2,22 +2,31 @@ import { useEffect,useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { SearchBook } from "./components/SearchBook";
+import { Pagination } from "../Utils/Pagination";
 export const SearchBooksPage= () => {
     const [books,setBooks] = useState<BookModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [booksPerPage] = useState(5);
+    const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect(()=> {
         const fetchBooks = async () => {
             const baseUrl : string = "http://localhost:8080/api/books";
 
-            const url : string = `${baseUrl}?page=0&size=5`;
+            const url : string = `${baseUrl}?page=${currentPage-1}&size=${booksPerPage}`;
             const response = await fetch(url);
             if(!response.ok){
                 throw new Error('Something went wrong!');
             }
             const responseJson = await response.json();
             const responseData = responseJson._embedded.books;
+
+            setTotalAmountOfBooks(responseJson.page.totalElements);
+            setTotalPages(responseJson.page.totalPages);
+
             const loadedBooks: BookModel[] = [];
             for(const key in responseData){
                 loadedBooks.push({
@@ -38,7 +47,8 @@ export const SearchBooksPage= () => {
             setIsLoading(false);
             setHttpError(error.message);
         })
-    },[]);
+        window.scrollTo(0,0);
+    },[currentPage]);
     if(isLoading){
         return(
             <SpinnerLoading/>
@@ -51,6 +61,12 @@ export const SearchBooksPage= () => {
             </div>
         )
     }
+    const indexOfLastBook : number = currentPage * booksPerPage;
+    const indexOfFirstBook : number = indexOfLastBook - booksPerPage;
+    let lastItem = booksPerPage* currentPage <= totalAmountOfBooks ? booksPerPage * currentPage : totalAmountOfBooks;
+
+    const paginate = (pageNumber:number) => setCurrentPage(pageNumber);
+
     return(
         <div>
             <div className="container">
@@ -103,14 +119,17 @@ export const SearchBooksPage= () => {
                         </div>
                     </div>
                     <div className="mt-3">
-                        <h5> Number of Results: (22)</h5>
+                        <h5> Number of Results: ({totalAmountOfBooks})</h5>
                     </div>
                     <p>
-                        1 to 5 of 22 items:
+                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
                     </p>
                     {books.map(book => (
                         <SearchBook book={book} key={book.id}/>
                     ))}
+                    {totalPages > 1 && 
+                        <Pagination currentPage = {currentPage} totalPages = {totalPages} paginate={paginate}/>
+                    }
                 </div>
             </div>
         </div>
