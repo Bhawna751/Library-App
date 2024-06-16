a88758d5fb6b20b1410e35e72ab6584b04dddc3f
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-16 06:26:02 +0530

    Home Page - Loading and Error Render, Loading Spinners, ReturnBook props,

--- 02-frontend/react-library/src/layouts/HomePage/Components/Carousel.tsx
+++ 02-frontend/react-library/src/layouts/HomePage/Components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ReturnBook } from "./ReturnBook";
 import { useEffect,useState} from "react";
 import BookModel from "../../../models/BookModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 export const Carousel = () => {
     const [books,setBooks] = useState<BookModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
@@ -39,6 +40,18 @@ export const Carousel = () => {
         })
     },[]);
 
+    if(isLoading){
+        return(
+            <SpinnerLoading/>
+        )
+    }
+    if(httpError){
+        return(
+            <div className="container m-5">
+                <p>{httpError}</p>
+            </div>
+        )
+    }
     return (
         <div className="container mt-5" style={{ height: 550 }}>
             <div className="homepage-carousel-title">
@@ -51,25 +64,25 @@ export const Carousel = () => {
                 <div className="carousel-inner ">
                     <div className="carousel-item active">
                         <div className="row d-flex justify-content-center align-item-center">
-                            <ReturnBook/>
-                            <ReturnBook/>
-                            <ReturnBook/>
+                            {books.slice(0,3).map(book=>(
+                                <ReturnBook book = {book} key = {book.id} />
+                            ))}
                         </div>
                     </div>
 
                     <div className="carousel-item ">
                         <div className="row d-flex justify-content-center align-item-center">
-                            <ReturnBook/>
-                            <ReturnBook/>
-                            <ReturnBook/>
+                            {books.slice(3,6).map(book=>(
+                                <ReturnBook book = {book} key = {book.id} />
+                            ))}
                         </div>
                     </div>
 
                     <div className="carousel-item ">
                         <div className="row d-flex justify-content-center align-item-center">
-                            <ReturnBook/>
-                            <ReturnBook/>
-                            <ReturnBook/>
+                            {books.slice(6,9).map(book=>(
+                                <ReturnBook book = {book} key = {book.id} />
+                            ))}
                         </div>
                     </div>
 
@@ -91,7 +104,7 @@ export const Carousel = () => {
             {/*Mobile*/}
             <div className="d-lg-none mt-3">
                 <div className="row d-flex justify-content-center align-items-center">
-                    <ReturnBook/>
+                    <ReturnBook book={books[7]} key={books[7].id}/>
                 </div>
             </div>
             <div className="homepage-carousel-title mt-3">
