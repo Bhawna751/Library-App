e739a4535f605a60e3178b33a09fd911c4a4a656
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-16 11:18:19 +0530

    Search Books Page

--- 02-frontend/react-library/src/App.tsx
+++ 02-frontend/react-library/src/App.tsx
@@ -4,12 +4,15 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBook } from './layouts/SearchBooksPage/components/SearchBook';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar/>
-      <HomePage/>
+      {/*<HomePage/>*/}
+      <SearchBooksPage/>
       <Footer/>
     </div>
   );
