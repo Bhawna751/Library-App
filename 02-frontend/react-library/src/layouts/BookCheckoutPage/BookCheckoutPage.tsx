312065c6dffb4a570c24743017caaeb2a715c72d
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-06-24 19:40:50 +0530

    React Reviews

--- 02-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 02-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -6,6 +6,9 @@ import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 import ReviewModel from "../../models/ReviewModel";
 import { LatestReviews } from "./LatestReviews";
 import { useOktaAuth } from "@okta/okta-react";
+import { request } from "http";
+import ReviewRequestModel from "../../models/ReviewRequestModel";
+
 
 export const BookCheckoutPage = () => {
 
@@ -20,6 +23,9 @@ export const BookCheckoutPage = () => {
     const [reviews, setReviews] = useState<ReviewModel[]>([])
     const [totalStars, setTotalStars] = useState(0);
     const [isLoadingReview,setIsLoadingReview] = useState(true);
+
+    const [isReviewLeft, setIsReviewLeft] = useState(false);
+    const [isLoadingUserReview, setIsLoadingUserReview] = useState(true);
 
     //Loans Count State
     const [currentLoansCount,setCurrentLoansCount] = useState(0);
@@ -101,7 +107,35 @@ export const BookCheckoutPage = () => {
             setIsLoadingReview(false);
             setHttpError(error.message);
         })
-    },[]);
+    },[isReviewLeft]);
+
+    useEffect(()=>{
+        const fetchUserReviewBook= async()=>{
+            if(authState && authState.isAuthenticated){
+                const url = `http://localhost:8080/api/secure/user/book/?bookId=${bookId}`;
+                const requestOptions={
+                    method:'GET',
+                    headers:{
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        'Content-Type':'application/json'
+                    }
+                };
+                const userReview = await fetch(url, requestOptions);
+                if(!userReview.ok){
+                    throw new Error('Something went wrong');
+
+                }
+                const userReviewResponseJson = await userReview.json();
+                setIsReviewLeft(userReviewResponseJson);
+            }
+            setIsLoadingUserReview(false);
+        }
+        fetchUserReviewBook().catch((error:any)=>{
+            setIsLoadingUserReview(false);
+            setHttpError(error.message);
+        })
+
+    },[authState]);
 
     useEffect(()=>{
         const fetchUserCurrentLoansCount = async () => {
@@ -155,7 +189,7 @@ export const BookCheckoutPage = () => {
             setHttpError(error.message);
         })
     },[authState]);
-    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingBookCheckedOut) {
+    if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingBookCheckedOut || isLoadingUserReview) {
         return (
             <SpinnerLoading />
         )
@@ -186,6 +220,28 @@ export const BookCheckoutPage = () => {
         setIsCheckedOut(true);
     }
 
+    async function submitReview(starInput: number, reviewDescription:string){
+        let bookId: number = 0;
+        if(book?.id){
+            bookId = book.id;
+        }
+        const reviewRequestModel = new ReviewRequestModel(starInput, bookId, reviewDescription);
+        const url = `http://localhost:8080/api/reviews/secure`;
+        const requestOptions={
+            method:'POST',
+            headers: {
+                Authorization:`Bearer ${authState?.accessToken?.accessToken}`,
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(reviewRequestModel)
+        };
+        const returnResponse= await fetch(url, requestOptions);
+        if(!returnResponse.ok){
+            throw new Error('Something went wrong!');
+        }
+        setIsReviewLeft(true);
+    }
+
     return (
         <div>
             <div className="container d-none d-lg-block">
@@ -210,7 +266,7 @@ export const BookCheckoutPage = () => {
                     </div>
                     <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount} 
                         isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
-                        checkoutBook={checkoutBook}/>
+                        checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} submitReview={submitReview}/>
                 </div>
                 <hr/>
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -236,7 +292,7 @@ export const BookCheckoutPage = () => {
                 </div>
                 <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount} 
                     isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
-                    checkoutBook={checkoutBook}/>
+                    checkoutBook={checkoutBook} isReviewLeft={isReviewLeft} submitReview={submitReview}/>
                 <hr />
                 <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
             </div>
--- 02-frontend/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ 02-frontend/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
 import BookModel from "../../models/BookModel"
+import { LeaveAReview } from "../Utils/LeaveAReview"
 
 export const CheckoutAndReviewBox: React.FC<{book:BookModel | undefined, mobile: boolean,
-    currentLoansCount: number , isAuthenticated:any, isCheckedOut:boolean, checkoutBook:any}> = (props) => {
+    currentLoansCount: number , isAuthenticated:any, isCheckedOut:boolean, checkoutBook:any,
+    isReviewLeft:boolean ,submitReview:any}> = (props) => {
 
     function buttonRender(){
         if(props.isAuthenticated){
@@ -16,6 +18,25 @@ export const CheckoutAndReviewBox: React.FC<{book:BookModel | undefined, mobile:
         }
         return (<Link to={'/login'} className="btn btn-success btn-lg">Sign in</Link>)
     }
+
+    function reviewRender(){
+        if(props.isAuthenticated && !props.isReviewLeft){
+            return (
+            <p>
+                <LeaveAReview submitReview={props.submitReview}/>
+            </p>)
+        }
+        else if(props.isAuthenticated && props.isReviewLeft){
+            return (<p><b>Thank you for your review!</b></p>)
+        }
+        return (
+            <div>
+                <hr/>
+                <p>Sign in to be able to leave a review.</p>
+            </div>
+        )
+    }
+    
 
     return(
         <div className={props.mobile ? 'card d-flex mt-5' : 'container d-flex mb-5'}>
@@ -49,9 +70,7 @@ export const CheckoutAndReviewBox: React.FC<{book:BookModel | undefined, mobile:
                 <p className="mt-3">
                     This number can change until placing order has been complete.
                 </p>
-                <p>
-                    Sign in to be able to leave a review.
-                </p>
+                {reviewRender()}
             </div>
         </div>
     )
