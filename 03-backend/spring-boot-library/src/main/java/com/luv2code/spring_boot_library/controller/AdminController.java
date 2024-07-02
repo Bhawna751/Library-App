b4c91d09a48545ccb1c7a14c0bce91b2c574ff9e
Bhawna Pillai <143238251+Bhawna751@users.noreply.github.com>
2024-07-02 09:45:48 +0530

    Admin add a new book

---
+++ 03-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.luv2code.spring_boot_library.controller;
+
+import com.luv2code.spring_boot_library.requestmodels.AddBookRequest;
+import com.luv2code.spring_boot_library.service.AdminService;
+import com.luv2code.spring_boot_library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+    private AdminService adminService;
+    @Autowired
+    public AdminController(AdminService adminService){
+        this.adminService = adminService;
+    }
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value="Authorization") String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception{
+        String admin = ExtractJWT.payloadJwtExtraction(token,"\"userType\"");
+        if(admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+}
---
+++ 03-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/service/AdminService.java
@@ -0,0 +1,33 @@
+package com.luv2code.spring_boot_library.service;
+
+import com.luv2code.spring_boot_library.dao.BookRepository;
+import com.luv2code.spring_boot_library.entity.Book;
+import com.luv2code.spring_boot_library.requestmodels.AddBookRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class AdminService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public AdminService(BookRepository bookRepository){
+        this.bookRepository=bookRepository;
+    }
+
+    public void postBook(AddBookRequest addBookRequest){
+        Book book = new Book();
+        book.setTitle(addBookRequest.getTitle());
+        book.setAuthor(addBookRequest.getAuthor());
+        book.setDescription(addBookRequest.getDescription());
+        book.setCopiesAvailable(addBookRequest.getCopies());
+        book.setCopies(addBookRequest.getCopies());
+        book.setCategory(addBookRequest.getCategory());
+        book.setImg(addBookRequest.getImg());
+        bookRepository.save(book);
+    }
+
+}
